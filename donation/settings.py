7225b5a8d23ee52853835f2230ab5f3b92af4e09
kodhanjo <felix.osambo@student.moringaschool.com>
2021-10-08 06:51:13 +0300

    added user models and created django admin

--- donation/settings.py
+++ donation/settings.py
@@ -15,7 +15,7 @@ from os import environ
 
 from pathlib import Path
 
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
+    'donation.apps.DonationConfig',
+    'user_app.apps.UserAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,8 +82,10 @@ WSGI_APPLICATION = 'donation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'donation',
+        'USER': 'moringa',
+    'PASSWORD':'kodhanjo',
     }
 }
 
@@ -110,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.intagerField(primary_key=True)
+    username = models.CharField(max_length=100)
+    phone_number = models.intagerField(max_length=10,blank =False)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+
