cbd647f1823058dfcea36fa6c932dd950f934637
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-10-15 06:37:05 +0300

    added view functionality

--- beneficiaries/views.py
+++ beneficiaries/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import statistics
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializer import BeneficiarySerializer
 
 # Create your views here.
+
+class BeneficiaryList(APIView):
+    def get(self, request, format=None):
+        Beneficiiaries = Beneficiary.objects.all()
+        serializers = BeneficiarySerializer(projects, many=True)
+        return Response(serializers.data)
+
+def post(self):
+    pass  
